9e30bdeb5a8d13d199760411f7b0298d38487c3f
Patryk Popiela <patryk.popiela01@gmail.com>
2024-10-25 22:55:16 +0200

    Add side navigation

--- components/theme/ThemeSwitch.tsx
+++ components/theme/ThemeSwitch.tsx
@@ -1,19 +1,35 @@
-"use client";
+'use client';
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
 
-import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 export default function ThemeSwitch() {
-      const [isMounted, setIsMounted] = useState(false);
-      const {setTheme, resolvedTheme} = useTheme();
+	const [isMounted, setIsMounted] = useState(false);
+	const { setTheme, resolvedTheme } = useTheme();
 
-        useEffect(() => {
-                setIsMounted(true)
-        }, [])
+	useEffect(() => {
+		setIsMounted(true);
+	}, []);
 
-    if(!isMounted) return null;
+	if (!isMounted) return null;
 
-    if(resolvedTheme === 'dark') return <button onClick={() => setTheme('light')}>Light</button>
+	if (resolvedTheme === 'dark')
+		return (
+			<button
+				className='block p-4 rounded-2xl hover:bg-blue-400 transition-colors duration-200 '
+				onClick={() => setTheme('light')}
+			>
+				<FaSun className='mx-auto' />
+			</button>
+		);
 
-    return <button onClick = {() => setTheme('dark')}>Black</button>
-} 
+	return (
+		<button
+			className='block p-4 rounded-2xl hover:bg-blue-400 transition-colors duration-200'
+			onClick={() => setTheme('dark')}
+		>
+			<FaMoon className='mx-auto' />
+		</button>
+	);
+}
