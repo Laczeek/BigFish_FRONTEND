f7cac180807df1bc7748679bfeec5f9b690d2c9e
Patryk Popiela <patryk.popiela01@gmail.com>
2024-11-05 16:16:46 +0100

    Modify HomePage

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image';
+
 import AnglersList from '@/components/home-page/AnglersList';
 import FishList from '@/components/home-page/FishList';
+
+import artImage from '@/public/images/art.jpg';
 
 export default function HomePage() {
 	return (
 		<>
 			<h1 className='text-3xl mb-6 font-bold'>Home</h1>
+			<p className='text-xs bg-warningYellow w-fit p-2 text-black rounded-md'>
+				The result will be different every day
+			</p>
 			<section>
 				<h2 className='text-2xl font-bold text-center tracking-wider m-0'>
 					From your country
@@ -27,9 +34,14 @@ export default function HomePage() {
 			</section>
 
 			<section className='mt-20'>
-				<h2 className='text-2xl font-bold text-center tracking-wider'>
-					From around the world
-				</h2>
+				<div
+					className='flex justify-center items-center z-0 relative h-[250px] after:absolute after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-black after:-z-10 after:bg-opacity-80 rounded-lg overflow-hidden bg-fixed bg-cover bg-bottom mb-1'
+					style={{ backgroundImage: `url(${artImage.src})` }}
+				>
+					<h2 className='text-2xl font-bold text-center tracking-wider z-10'>
+						From around the world
+					</h2>
+				</div>
 
 				<section className='p-2'>
 					<h3 className='text-xl mb-4 text-center'>
