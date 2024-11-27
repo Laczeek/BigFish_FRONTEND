6cf8640d29111544acda234432a29a47737f4b10
Patryk Popiela <patryk.popiela01@gmail.com>
2024-11-27 22:52:54 +0100

    Add basic ErrorPage

---
+++ app/error.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import GoBackButton from '@/components/layout-related/GoBackButton';
+
+export default function ErrorPage({ error }: { error: Error }) {
+	return (
+		<div className='h-full flex items-start'>
+			<GoBackButton />
+
+			<div className='self-center text-center grow'>
+				<p className='text-8xl mb-4 tracking-wider text-light-accentPrimary dark:text-dark-accentPrimary'>
+					Oops!
+				</p>
+				<h2 className='text-4xl trackng-widest'>
+					{error.message || 'Something went wrong.'}
+				</h2>
+			</div>
+		</div>
+	);
+}
