940bf8b30561bd6e61db059394824a79de1e5b12
Patryk Popiela <patryk.popiela01@gmail.com>
2024-12-27 22:01:51 +0100

    Remove useEffect hook from providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,13 @@
 'use client';
 
-import { ReactNode, useEffect } from 'react';
+import { ReactNode } from 'react';
 import { ThemeProvider } from 'next-themes';
 import { Provider } from 'react-redux';
 
 import store from '@/store/store';
 import ModalsContainer from '@/components/modals/ModalsContainer';
 
-
 export default function Providers({ children }: { children: ReactNode }) {
-
 	return (
 		<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
 			<Provider store={store}>
